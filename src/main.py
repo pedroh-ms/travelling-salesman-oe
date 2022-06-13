8b59fb392922c38b34ba05b31e2dd2b0b4395a46
Pedro de Souza <pedromartins12117@gmail.com>
2022-06-13 08:40:51 -0300

    requirements.txt added

--- src/main.py
+++ src/main.py
@@ -22,33 +22,21 @@ NETWORK_TYPE = 'walk' # (string {"all_private", "all", "bike", "drive", "drive_s
 START_PLACE = "Universidade Federal de Catalão"
 END_PLACE = "Universidade Federal de Catalão"
 
-TEST_SAMPLES = [
-[10, 2, 0, 1, 9, 4, 3, 7, 5, 8, 6, 10],
-[10, 0, 1, 5, 4, 8, 7, 2, 9, 6, 3, 10],
-[10, 8, 0, 6, 2, 4, 9, 7, 3, 5, 1, 10],
-[10, 3, 1, 4, 9, 0, 8, 2, 5, 7, 6, 10],
-[10, 8, 9, 2, 0, 7, 6, 4, 1, 5, 3, 10],
-[10, 4, 6, 1, 9, 8, 3, 2, 0, 7, 5, 10],
-[10, 4, 6, 1, 5, 9, 8, 3, 2, 7, 0, 10],
-[10, 9, 6, 3, 2, 4, 7, 5, 8, 1, 0, 10],
-[10, 7, 3, 2, 1, 4, 5, 6, 0, 9, 8, 10],
-[10, 4, 9, 2, 8, 3, 7, 1, 5, 0, 6, 10],
-[10, 4, 5, 6, 7, 2, 9, 3, 1, 8, 0, 10],
-[10, 2, 5, 9, 8, 7, 4, 0, 1, 6, 3, 10]
-]
+
+def printBestRoute(route):
+    print('Best solution found:')
+    print(f'Route -> {route.places}')
+    print(f'Cost -> {route.cost}')    
 
 
+def sa_test():
 
-def main():
-
-    print('Hello world!')
+    print('Travelling salesman solved with simulated annealing...')
     lc_graph = LocationGraph(LOCATION, PLACES, NETWORK_TYPE)
 
     PLACES_KEYS = list(PLACES.keys())
 
     rt = lc_graph.newRoute(PLACES_KEYS.index(START_PLACE), PLACES_KEYS.index(END_PLACE))
-
-    print(rt.places, rt.cost)
 
     model = lc_graph.travellingSalesman(PLACES_KEYS.index(START_PLACE), 'SA',
                                         num_cycles=100,
@@ -56,34 +44,27 @@ def main():
                                         init_temp=200)
 
     model.run(rt)
-
-    print(model.best_route.places, model.best_route.cost)
+    printBestRoute(model.best_route)
 
     route_map = lc_graph.createRouteMap(model)
-    route_map.save('optimal_route.html')
+    route_map.save('optimal_route_SA.html')
     
+
 def ga_test():
 
-    print('Hello world!')
+    print('Travelling salesman solved with genetic algorithm...')
     lc_graph = LocationGraph(LOCATION, PLACES, NETWORK_TYPE)
     
     PLACES_KEYS = list(PLACES.keys())
-    
-    rt_1 = lc_graph.newRoute(PLACES_KEYS.index(START_PLACE), PLACES_KEYS.index(END_PLACE), [10, 2, 3, 5, 0, 7, 4, 9, 6, 1, 8, 10])
-    rt_2 = lc_graph.newRoute(PLACES_KEYS.index(START_PLACE), PLACES_KEYS.index(END_PLACE), [10, 7, 5, 3, 9, 4, 0, 1, 6, 8, 2, 10])
-    
-    initial_pop = []
-    for i in TEST_SAMPLES:
-        initial_pop.append(lc_graph.newRoute(PLACES_KEYS.index(START_PLACE), PLACES_KEYS.index(END_PLACE), i))
-        
+           
     model = lc_graph.travellingSalesman(PLACES_KEYS.index(START_PLACE), 'GA',
-                                        num_generations=50,
-                                        population_size=71,
+                                        num_generations=5000,
+                                        population_size=100,
                                         mutation_prob=0.1)
                                     
 
     model.run()
-    print(model.best_route.places, model.best_route.cost)
+    printBestRoute(model.best_route)
     
     route_map = lc_graph.createRouteMap(model)
     route_map.save('optimal_route_GA.html')
@@ -91,6 +72,6 @@ def ga_test():
 
 if __name__ == '__main__':
 
-    # main()
-    ga_test()
+    sa_test()
+    # ga_test()
 
